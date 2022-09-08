cb6e2ff2b21145826cd36c41bde4e8c3ea6f3c49
WardLavrijsen <70542124+WardLavrijsen@users.noreply.github.com>
2022-09-08 14:33:06 +0200

    fixed it

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Firebase
+          Learn React
         </a>
       </header>
     </div>
