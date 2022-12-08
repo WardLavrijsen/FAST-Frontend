a54eedadfdd3e21db40921392fc2a2474d93caa5
WardLavrijsen <70542124+WardLavrijsen@users.noreply.github.com>
2022-12-08 14:34:08 +0100

    cypress test local

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,6 @@
 describe("Test test", () => {
   it("passes", () => {
-    cy.visit("https://example.cypress.io");
-    cy.contains("type").click();
+    cy.visit("http://localhost:3000");
+    cy.contains("La Liga").click();
   });
 });
