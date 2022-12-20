04331965169373c165784227c7789d00d80005c3
WardLavrijsen <70542124+WardLavrijsen@users.noreply.github.com>
2022-12-20 09:49:50 +0100

    commit

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,3 +4,12 @@ describe("Is LaLiga available", () => {
     cy.contains("La Liga").click();
   });
 });
+
+// describe("Find new competiton", () => {
+//   it("passes", () => {
+//     cy.visit("http://localhost:3000");
+//     cy.contains("Competities").click();
+//     cy.contains("Eredivisie").click();
+//     cy.get("#grid-container").contains("Vitesse").click();
+//   });
+// });
